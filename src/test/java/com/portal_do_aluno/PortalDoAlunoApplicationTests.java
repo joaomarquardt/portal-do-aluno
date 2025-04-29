3c9531034afba98d2e8adf4d7b8c683a39326473
joaomarquardt <marquardt1402@gmail.com>
2025-04-29 13:38:43 -0300

    first commit

---
+++ src/test/java/com/portal_do_aluno/PortalDoAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal_do_aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalDoAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
