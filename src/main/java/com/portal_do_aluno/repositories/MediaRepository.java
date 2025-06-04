86b126af59bf2bdb0fdeb4cd593d7205197475a7
joaomarquardt <marquardt1402@gmail.com>
2025-06-04 15:28:48 -0300

    feat: add method to find grade and presence by student and class

--- src/main/java/com/portal_do_aluno/repositories/MediaRepository.java
+++ src/main/java/com/portal_do_aluno/repositories/MediaRepository.java
@@ -1,7 +1,12 @@
 package com.portal_do_aluno.repositories;
 
+import com.portal_do_aluno.domain.Aluno;
 import com.portal_do_aluno.domain.Media;
+import com.portal_do_aluno.domain.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MediaRepository extends JpaRepository<Media, Long> {
+    Optional<Media> findByAlunoAndTurma(Aluno aluno, Turma turma);
 }
--- src/main/java/com/portal_do_aluno/repositories/PresencaRepository.java
+++ src/main/java/com/portal_do_aluno/repositories/PresencaRepository.java
@@ -1,7 +1,12 @@
 package com.portal_do_aluno.repositories;
 
+import com.portal_do_aluno.domain.Aluno;
 import com.portal_do_aluno.domain.Presenca;
+import com.portal_do_aluno.domain.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PresencaRepository extends JpaRepository<Presenca, Long> {
+    Optional<Presenca> findByAlunoAndTurma(Aluno aluno, Turma turma);
 }
