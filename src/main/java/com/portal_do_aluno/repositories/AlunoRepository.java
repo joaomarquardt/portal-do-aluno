18ca32e43b25a4c7f09ca576c5b7b94678d40047
joaomarquardt <marquardt1402@gmail.com>
2025-06-04 16:30:23 -0300

    feat: implement useful methods in student repository

--- src/main/java/com/portal_do_aluno/repositories/AlunoRepository.java
+++ src/main/java/com/portal_do_aluno/repositories/AlunoRepository.java
@@ -1,7 +1,14 @@
 package com.portal_do_aluno.repositories;
 
 import com.portal_do_aluno.domain.Aluno;
+import com.portal_do_aluno.domain.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    int countByCursoAndPeriodoIngresso(Curso curso, String periodoIngresso);
+
+    Optional<Aluno> findByMatricula(String matricula);
 }
