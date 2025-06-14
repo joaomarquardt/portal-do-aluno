0a95ae14bfac2f01b01c62e788848a496e7699b0
dropepaulo <pedroprds.dev@gmail.com>
2025-06-14 20:35:36 -0300

    Language change from Portuguese to English

---
+++ backend/src/main/java/com/portal_do_aluno/domain/Student.java
@@ -0,0 +1,47 @@
+package com.portal_do_aluno.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String registrationNumber;
+
+    private int periodoAtual = 1;
+    private String enrollmentSemester;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private course course;
+
+    @ManyToMany(mappedBy = "alunos")
+    private List<Turma> classGroups;
+    private boolean enrolled= true;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Student(String registrationNumber, String enrollmentSemester, course course, List<Turma> classGroups, user user) {
+        this.registrationNumber = registrationNumber;
+        this.enrollmentSemester = enrollmentSemester;
+        this.course = course;
+        this.classGroups = classGroups;
+        this.user = user;
+    }
+}
