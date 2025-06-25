933fea13d761bbeba555eaa741033d6569275b19
joaomarquardt <marquardt1402@gmail.com>
2025-06-25 00:42:43 -0300

    feat: add methods to help and improve authentication logic

--- backend/src/main/java/com/portal_do_aluno/repositories/UsuarioRepository.java
+++ backend/src/main/java/com/portal_do_aluno/repositories/UsuarioRepository.java
@@ -3,5 +3,10 @@ package com.portal_do_aluno.repositories;
 import com.portal_do_aluno.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCpf(String cpf);
+
+    Optional<Usuario> findByEmailInstitucional(String emailInstitucional);
 }
--- backend/src/main/java/com/portal_do_aluno/services/UsuarioService.java
+++ backend/src/main/java/com/portal_do_aluno/services/UsuarioService.java
@@ -3,7 +3,6 @@ package com.portal_do_aluno.services;
 import com.portal_do_aluno.domain.Aluno;
 import com.portal_do_aluno.domain.Professor;
 import com.portal_do_aluno.domain.Usuario;
-import com.portal_do_aluno.dtos.requests.CreateUsuarioRequestDTO;
 import com.portal_do_aluno.dtos.requests.UpdateUsuarioRequestDTO;
 import com.portal_do_aluno.dtos.responses.AlunoResponseDTO;
 import com.portal_do_aluno.dtos.responses.ProfessorResponseDTO;
@@ -12,8 +11,12 @@ import com.portal_do_aluno.mappers.AlunoMapper;
 import com.portal_do_aluno.mappers.ProfessorMapper;
 import com.portal_do_aluno.mappers.UsuarioMapper;
 import com.portal_do_aluno.repositories.UsuarioRepository;
+import com.portal_do_aluno.security.dtos.requests.RegisterRequestDTO;
+import com.portal_do_aluno.security.exceptions.RegisterConflictException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,11 +46,17 @@ public class UsuarioService {
     }
 
     public UsuarioResponseDTO findById(Long id) {
-        Usuario entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado!"));
+        Usuario entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
         return mapper.toResponseDTO(entidade);
     }
 
-    public UsuarioResponseDTO create(CreateUsuarioRequestDTO usuarioDTO) {
+    public UsuarioResponseDTO create(RegisterRequestDTO usuarioDTO) {
+        if (repository.findByCpf(usuarioDTO.cpf()).isPresent()) {
+            throw new RegisterConflictException("CPF já vinculado a usuário.");
+        }
+        if (repository.findByEmailInstitucional(usuarioDTO.emailInstitucional()).isPresent()) {
+            throw new RegisterConflictException("Email institucional já vinculado a usuário.");
+        }
         Usuario entidade = mapper.toEntity(usuarioDTO);
         if (usuarioDTO.aluno() != null) {
             AlunoResponseDTO alunoCriado = alunoService.create(usuarioDTO.aluno());
@@ -66,7 +75,7 @@ public class UsuarioService {
     }
 
     public UsuarioResponseDTO update(Long id, UpdateUsuarioRequestDTO usuarioDTO) {
-        Usuario entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado!"));
+        Usuario entidade = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado!"));
         mapper.updateEntityFromDTO(usuarioDTO, entidade);
         entidade = repository.save(entidade);
         return mapper.toResponseDTO(entidade);
@@ -74,5 +83,10 @@ public class UsuarioService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    private Usuario getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (Usuario) authentication.getPrincipal();
     }
 }
