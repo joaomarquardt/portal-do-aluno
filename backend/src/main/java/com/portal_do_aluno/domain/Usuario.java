dfaa44823665a628011715c407e87cd4c00fa2b2
joaomarquardt <marquardt1402@gmail.com>
2025-06-25 00:04:41 -0300

    feat: add userdetails security interface to deal with authentication and permissions

--- backend/src/main/java/com/portal_do_aluno/domain/Usuario.java
+++ backend/src/main/java/com/portal_do_aluno/domain/Usuario.java
@@ -1,10 +1,18 @@
 package com.portal_do_aluno.domain;
 
+import com.portal_do_aluno.security.domain.PapelUsuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -12,7 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,6 +33,12 @@ public class Usuario {
     private String emailInstitucional;
     private String telefone;
     private String senha;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "usuario_papeis", joinColumns = @JoinColumn(name = "usuario_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "papel")
+    private List<PapelUsuario> papeis;
 
     @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
     private Aluno aluno;
@@ -41,5 +55,42 @@ public class Usuario {
         this.senha = senha;
         this.aluno = aluno;
         this.professor = professor;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return papeis.stream()
+                .map(papel -> new SimpleGrantedAuthority("ROLE_" + papel.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return cpf;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
