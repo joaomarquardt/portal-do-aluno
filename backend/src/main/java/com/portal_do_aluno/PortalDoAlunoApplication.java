48c8d91413c952d6361b518dee7e1b222e68ddff
joaomarquardt <marquardt1402@gmail.com>
2025-06-27 11:57:56 -0300

    feat: enable caching annotation in runnable file

--- backend/src/main/java/com/portal_do_aluno/PortalDoAlunoApplication.java
+++ backend/src/main/java/com/portal_do_aluno/PortalDoAlunoApplication.java
@@ -2,8 +2,10 @@ package com.portal_do_aluno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PortalDoAlunoApplication {
 
 	public static void main(String[] args) {
