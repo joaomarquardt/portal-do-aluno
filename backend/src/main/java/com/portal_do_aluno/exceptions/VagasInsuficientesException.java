c3aa6a56dad78461920899146e648d85b9d988c0
dropepaulo <pedroprds.dev@gmail.com>
2025-07-01 02:07:02 -0300

    feat: added global error handler. replacing two files with one centralizing everything.

--- backend/src/main/java/com/portal_do_aluno/exceptions/VagasInsuficientesException.java
+++
@@ -1,7 +0,0 @@
-package com.portal_do_aluno.exceptions;
-
-public class VagasInsuficientesException extends RuntimeException {
-    public VagasInsuficientesException(String message) {
-        super(message);
-    }
-}
