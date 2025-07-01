47b4e0755ac9afeb64831b86f4f65f630359b956
dropepaulo <pedroprds.dev@gmail.com>
2025-07-01 01:42:35 -0300

    feat: adding validation to dto/requests using jakarta.validation

--- backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdateDesempenhoRequestDTO.java
+++ backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdateDesempenhoRequestDTO.java
@@ -1,7 +1,13 @@
 package com.portal_do_aluno.dtos.requests;
+import jakarta.validation.constraints.*;
 
 public record UpdateDesempenhoRequestDTO(
+        @DecimalMin(value = "0.0", message = "O valor não pode ser negativo.")
         double valor,
+
+        @Min(value = 0, message = "A presença não pode ser negativa.")
+        @Max(value = 100, message = "A presença máxima é 100.")
         int presenca
 ) {
 }
+
--- backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdateUsuarioRequestDTO.java
+++ backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdateUsuarioRequestDTO.java
@@ -1,8 +1,15 @@
 package com.portal_do_aluno.dtos.requests;
+import jakarta.validation.constraints.*;
 
 public record UpdateUsuarioRequestDTO(
+        @Email(message = "Email pessoal inválido.")
         String emailPessoal,
+
+        @Email(message = "Email institucional inválido.")
         String emailInstitucional,
+
+        @Pattern(regexp = "\\d{10,11}", message = "Telefone deve conter 10 ou 11 dígitos.")
         String telefone
 ) {
 }
+
