bc36f4367bf326ffe4fee557c34fb73b99d3b439
dropepaulo <pedroprds.dev@gmail.com>
2025-07-01 01:42:35 -0300

    feat: adding validation to dto/requests using jakarta.validation

--- backend/src/main/java/com/portal_do_aluno/dtos/requests/AddAlunosTurmaDTO.java
+++ backend/src/main/java/com/portal_do_aluno/dtos/requests/AddAlunosTurmaDTO.java
@@ -1,8 +1,10 @@
 package com.portal_do_aluno.dtos.requests;
-
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record AddAlunosTurmaDTO(
+        @NotEmpty(message = "A lista de alunos não pode estar vazia.")
         List<Long> idAlunos
 ) {
 }
+
--- backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdatePeriodoLetivoRequestDTO.java
+++ backend/src/main/java/com/portal_do_aluno/dtos/requests/UpdatePeriodoLetivoRequestDTO.java
@@ -1,9 +1,12 @@
 package com.portal_do_aluno.dtos.requests;
-
+import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 
 public record UpdatePeriodoLetivoRequestDTO(
         boolean ativo,
+
+        @NotNull(message = "A data de fim é obrigatória.")
         LocalDate dataFim
 ) {
 }
+
