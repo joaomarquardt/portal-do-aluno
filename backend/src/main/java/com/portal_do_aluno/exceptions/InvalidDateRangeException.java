b830c798672cc1fbabde754d0adff631d3e1671b
dropepaulo <pedroprds.dev@gmail.com>
2025-07-01 02:07:02 -0300

    feat: added global error handler. replacing two files with one centralizing everything.

--- backend/src/main/java/com/portal_do_aluno/exceptions/InvalidDateRangeException.java
+++
@@ -1,7 +0,0 @@
-package com.portal_do_aluno.exceptions;
-
-public class InvalidDateRangeException extends RuntimeException {
-    public InvalidDateRangeException(String message) {
-        super(message);
-    }
-}
