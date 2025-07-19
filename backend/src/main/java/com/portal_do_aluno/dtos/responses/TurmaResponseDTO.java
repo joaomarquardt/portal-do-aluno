f8251dc28dce9a3ee18cf095c5094ed53f97d8eb
rafaaeltavares <rafaeltavsilva@gmail.com>
2025-07-19 11:39:27 -0300

    feat: add stats on turma DTO to return if turma its active or not

--- backend/src/main/java/com/portal_do_aluno/dtos/responses/TurmaResponseDTO.java
+++ backend/src/main/java/com/portal_do_aluno/dtos/responses/TurmaResponseDTO.java
@@ -1,11 +1,14 @@
 package com.portal_do_aluno.dtos.responses;
 
+import com.portal_do_aluno.domain.TurmaStatus;
+
 public record TurmaResponseDTO(
         long id,
         String codigo,
         String periodo,
         int vagasTotais,
         String horario,
+        TurmaStatus status,
         DisciplinaResponseDTO disciplina,
         ProfessorResponseDTO professor
 ) {
